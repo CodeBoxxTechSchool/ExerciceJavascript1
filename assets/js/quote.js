49f30c1a969781d263feb17dd6f0de77b1d3c857
JGuibord <jeremyguibord@outlook.com>
2020-09-20 10:47:31 -0400

    10:47 on Sept. 20th - CORRECTION

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,53 +1,54 @@
 $(document).ready(function () {
     $("#numElev_2, #numElev_3, #elevPriceUnit, #elevTotal, #installationFee, #total_").attr('readonly', true);
-
     var numApp, numFloors, numBase, maxOcc;
     var prodRange = {
         type: null,
         price: null,
         installationFeePercentage: null
     };
-
     $('.formField').on('keyup', function () {
         doCalc();
     });
 
 
     $('#standart, #premium, #excelium').on('click', function () {
+    $('#standard').on('click', function () {
         document.getElementById('elevPriceUnit').value = (7565).toFixed(2) + " $";
+        doCalc();
+    });
+
+    $('#premium').on('click', function () {
+        document.getElementById('elevPriceUnit').value = (12345).toFixed(2) + " $";
+        doCalc();
+    });
+
+    $('#excelium').on('click', function () {
+        document.getElementById('elevPriceUnit').value = (15400).toFixed(2) + " $";
         doCalc();
     });
 
     $('#residential, #commercial, #corporate, #hybrid').on('click', function () {
         initialize();
     });
-
-
     function initialize() {
         $('.formField').val('');
         $('.productRangeBtn').prop('checked', false);
     };
-
     function getInfoNumApp() {
         numApp = $('#numApp').val();
     };
-
     function getInfoNumFloors() {
         numFloors = $('#numFloors').val();
     };
-
     function getInfoNumBase() {
         numBase = $('#numBase').val();
     };
-
     function getInfoNumElev() {
         numElev = $('#numElev').val();
     };
-
     function getInfoMaxOcc() {
         maxOcc = $('#maxOcc').val();
     };
-
     function getProdRange() {
         if ($('#standard').is(':checked')) {
             prodRange.type = "standard";
@@ -58,6 +59,7 @@ $(document).ready(function () {
         } else if ($('#premium').is(':checked')) {
             prodRange.type = "premium";
             prodRange.price = parseFloat(123456);
+            prodRange.price = parseFloat(12345);
             prodRange.installationFeePercentage = 0.13;
             return prodRange;
 
@@ -75,17 +77,16 @@ $(document).ready(function () {
     };
 
     function GetInfos() {
+        getInfoNumApp();
         getInfoNumFloors();
         getInfoNumBase();
         getInfoNumElev();
         getInfoMaxOcc();
         getProdRange();
     };
-
     function setRequiredElevatorsResult(finNumElev) {
         $("#numElev_2, #numElev_3").val(parseFloat(finNumElev));
     };
-
     function setPricesResults(finNumElev, roughTotal, installFee, total) {
         $("#elevTotal").val(parseFloat(roughTotal).toFixed(2) + " $");
         $("#installationFee").val(parseFloat(installFee).toFixed(2) + " $");
@@ -94,11 +95,13 @@ $(document).ready(function () {
 
     function emptyElevatorsNumberAndPricesFields() {
         $('#numElev_3').val('');
+        $('#numElev_2 #numElev_3').val('');
         $('.priceField').val('');
     };
 
     function createFormData(projectType) {
         return {
+            numberElev: numElev,
             numberApp: numApp,
             numberFloors: numFloors,
             numberBase: numBase,
@@ -107,61 +110,48 @@ $(document).ready(function () {
             projectType: projectType
         }
     };
-
     function negativeValues() {
         if ($('#numApp').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#numApp').val('');
             return true
-
         } else if ($('#numBase').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#numBase').val('');
             return true
-
         } else if ($('#numComp').val() < 0) {
-
-            alert("Please enter a positive number!");
-            $('#numComp').val('');
-            return true
-
+@@ -130,26 +142,32 @@
         } else if ($('#numPark').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#numPark').val('');
             return true
-
         } else if ($('#numElev').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#numElev').val('');
             return true
-
         } else if ($('#numCorpo').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#numCorpo').val('');
             return true
-
         } else if ($('#maxOcc').val() < 0) {
-
             alert("Please enter a positive number!");
             $('#maxOcc').val('');
             return true
+
+        } else if ($('#numFloors').val() < 0) {
+            alert("Please enter a positive number!");
+            $('#numFloors').val('');
+            return true
+
         } else {
             return false
         }
     };
-
     function apiCall(projectType) {
         //Getting numbers from quote
         GetInfos();
-
         //Preparing data for Api call
         formData = createFormData(projectType)
-
         $.ajax({
             type: "POST",
             // url: 'http://localhost:3000/api/quoteCalculation/', //for local testing
@@ -182,9 +172,17 @@ $(document).ready(function () {
         if ($('#residential').hasClass('active') && !negativeValues() && $('#numApp').val() && $('#numFloors').val()) {
             apiCall('residential')
         } else if ($('#commercial').hasClass('active') && !negativeValues() && $('#numElev').val()  && $('#numPark').val()) {
+
+        } else if ($('#commercial').hasClass('active') && !negativeValues() && $('#numElev').val()) {
             apiCall('commercial')
+
         } else if ($('#corporate').hasClass('active') && !negativeValues() && $('#numFloors').val() && $('#numBase').val() && $('#maxOcc').val()) {
             apiCall('commercial')
+            apiCall('corporate')
+
+        } else if ($('#hybrid').hasClass('active') && !negativeValues() && $('#numFloors').val() && $('#numBase').val() && $('#maxOcc').val()) {
+            apiCall('hybrid')
+
         } else {
             emptyElevatorsNumberAndPricesFields();
         };
