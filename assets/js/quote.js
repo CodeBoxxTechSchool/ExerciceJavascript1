94d30b856edc8fa354c20e2941ec11e838eeb17f
johnnybigoo <jotalico@icloud.com>
2021-10-04 09:32:11 -0400

    first-commit-JonesT-branch

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -13,8 +13,18 @@ $(document).ready(function () {
     });
 
 
-    $('#standart, #premium, #excelium').on('click', function () {
+    $('#standard').on('click', function () {
         document.getElementById('elevPriceUnit').value = (7565).toFixed(2) + " $";
+        doCalc();
+    });
+
+    $('#premium').on('click', function () {
+        document.getElementById('elevPriceUnit').value = (12345).toFixed(2) + " $";
+        doCalc();
+    });
+
+    $('#excelium').on('click', function () {
+        document.getElementById('elevPriceUnit').value = (15400).toFixed(2) + " $";
         doCalc();
     });
 
